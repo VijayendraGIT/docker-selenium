e75d2f69cb95938bc15a66c251dd5846c11ccab9
VijayendraGIT <43878836+VijayendraGIT@users.noreply.github.com>
2020-05-01 19:27:10 +0530

    Added class files

---
+++ src/main/java/com/newtours/pages/FlightDetailsPage.java
@@ -0,0 +1,41 @@
+package com.newtours.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FlightDetailsPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(name = "passCount")
+    private WebElement passangers;
+
+    @FindBy(name = "findFlights")
+    private WebElement submitButton;
+
+    public FlightDetailsPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver,30);
+        PageFactory.initElements(driver,this);
+    }
+
+    public void selectPassangers(String noOfPassangers){
+        this.wait.until(ExpectedConditions.elementToBeClickable(passangers));
+        System.out.println("Passangers = "+ noOfPassangers);
+        Select select = new Select(passangers);
+        select.selectByValue(noOfPassangers);
+
+    }
+
+    public void goToFindFlightsPage(){
+        this.submitButton.click();
+    }
+
+
+}
---
+++ src/test/java/com/tests/BaseTest.java
@@ -0,0 +1,50 @@
+package com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+    protected WebDriver driver;
+    @BeforeTest
+    public void setupDriver(ITestContext ctx) throws MalformedURLException {
+
+        //BROWSER =>chrome /firefox
+        //HUB_HOST => localhost /10.0.1.3 /hostname
+
+        String host = "localhost";
+        DesiredCapabilities dc ;
+
+        if(System.getProperty("BROWSER") != null && System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
+            dc = DesiredCapabilities.firefox();
+        } else {
+            dc = DesiredCapabilities.chrome();
+        }
+
+
+        if(System.getProperty("HUB-HOST") != null){
+            host = System.getProperty("HUB_HOST");
+        }
+
+        String testName = ctx.getCurrentXmlTest().getName();
+
+        String completeUrl = "http://" + host + ":4444/wd/hub";
+        this.driver = new RemoteWebDriver(new URL(completeUrl),dc);
+        System.out.println("TestName :" + testName);
+        dc.setCapability("name",testName);
+       // System.setProperty("webdriver.chrome.driver","C:\\Users\\vijayendra.kumar\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        //this.driver = new ChromeDriver() ;
+    }
+
+    @AfterTest
+    public void quitDriver(){
+        this.driver.quit();
+    }
+}
